8cf202f74a875a25079c38b10f841191e7aed4ce
suraj <surajsonkamble202@gmail.com>
2025-09-26 17:30:18 +0530

    user service code implementation

--- src/main/java/com/fitness/userservice/dto/UserResponse.java
+++ src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -1,5 +1,27 @@
 package com.fitness.userservice.dto;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
 public class UserResponse {
+
+	private String id;
+
+	private String email;
+
+	@JsonIgnore
+	private String password;
+
+	private String firstName;
+
+	private String lastName;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/fitness/userservice/service/UserService.java
+++ src/main/java/com/fitness/userservice/service/UserService.java
@@ -1,5 +1,69 @@
 package com.fitness.userservice.service;
 
+import org.springframework.stereotype.Service;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+	private final UserRepository userRepository;
+
+	/**
+	 * @param request
+	 * @return
+	 */
+	public UserResponse register(RegisterRequest request) {
+
+		if (userRepository.existsByEmail(request.getEmail())) {
+
+			throw new RuntimeException("Email Aready Exist");
+
+		}
+
+		User user = new User();
+
+		user.setEmail(request.getEmail());
+		user.setFirstName(request.getFirstName());
+		user.setLastName(request.getLastName());
+		user.setPassword(request.getPassword());
+
+		User savedUser = userRepository.save(user);
+
+		UserResponse userResponse = new UserResponse();
+
+		userResponse.setId(savedUser.getId());
+		userResponse.setEmail(savedUser.getEmail());
+		userResponse.setPassword(savedUser.getPassword());
+		userResponse.setFirstName(savedUser.getFirstName());
+		userResponse.setLastName(savedUser.getLastName());
+		userResponse.setCreatedAt(savedUser.getCreatedAt());
+		userResponse.setUpdatedAt(savedUser.getUpdatedAt());
+
+		return userResponse;
+
+	}
+
+	public UserResponse getUserProfile(String userId) {
+
+		User user = userRepository.findById(userId).orElseThrow((() -> new RuntimeException("Id does not existed")));
+
+		UserResponse userResponse = new UserResponse();
+
+		userResponse.setId(user.getId());
+		userResponse.setEmail(user.getEmail());
+		userResponse.setFirstName(user.getFirstName());
+		userResponse.setLastName(user.getLastName());
+		userResponse.setCreatedAt(user.getCreatedAt());
+		userResponse.setUpdatedAt(user.getUpdatedAt());
+
+		return userResponse;
+	}
 
 }
